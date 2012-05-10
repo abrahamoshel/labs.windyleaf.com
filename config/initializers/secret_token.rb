b04c0e5606c8c100d404876edd79f3d413e85a36
Abraham Oshel <abraham.oshel@gmail.com>
2012-05-09 21:38:07 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LabsWindyleafCom::Application.config.secret_token = '4ee4d101e58e40bc5793ad17b3823b0e72741a8b59fa73da86dfcae2359890725a47e8c56fd3add7d2922b4844696cb2d1899832a817a3f215c57150407a41d8'
